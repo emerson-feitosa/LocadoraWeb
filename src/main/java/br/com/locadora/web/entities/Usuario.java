851599a11e7476c2bfb48ae346cf1df53c360b7b
Emerson <Emerson.fmarc@hotmail.com>
2024-09-21 18:04:22 -0300

    feat: criação do usuario

---
+++ src/main/java/br/com/locadora/web/entities/Usuario.java
@@ -0,0 +1,42 @@
+package br.com.locadora.web.entities;
+
+import br.com.locadora.web.enums.StatusUsuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String telefone;
+    
+    @Column(unique = true)
+    private String cnh;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusUsuario statusUsuario;
+}
---
+++ src/main/java/br/com/locadora/web/enums/StatusUsuario.java
@@ -0,0 +1,6 @@
+package br.com.locadora.web.enums;
+
+public enum StatusUsuario {
+    REGULAR,
+    DEVEDOR;
+}
