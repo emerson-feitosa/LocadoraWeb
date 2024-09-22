b010f538c04814eb636413dfa788fcaa9c0bf8e7
Emerson <Emerson.fmarc@hotmail.com>
2024-09-21 21:04:57 -0300

    feat: Implementação da entidade Locacao

---
+++ src/main/java/br/com/locadora/web/entities/Locacao.java
@@ -0,0 +1,56 @@
+package br.com.locadora.web.entities;
+
+
+import br.com.locadora.web.enums.StatusLocacao;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "locacoes")
+public class Locacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuaruo_id", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "veiculo_id", nullable = false)
+    private Veiculo veiculo;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate dataInicio;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+    private LocalTime horaInicio;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate dataFim;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+    private LocalTime horaFim;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusLocacao status;
+
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "financeiro_id", nullable = false)
+    private Financeiro financeiro;
+}
---
+++ src/main/java/br/com/locadora/web/enums/StatusLocacao.java
@@ -0,0 +1,6 @@
+package br.com.locadora.web.enums;
+
+public enum StatusLocacao {
+    CONCLUIDA,
+    EM_ANDAMENTO;
+}
